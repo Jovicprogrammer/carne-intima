3261e0282d9813a030c9a8ceb25a91b568f51a6e
vesquicio <victosousa92@gmail.com>
2025-12-24 09:38:22 -0300

    iniciando o projeto

---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import localFont from 'next/font/local';
+
+
+// Fonte 2 - Exemplo simples
+export const Impact = localFont({
+  src: './fonts/impact.ttf',
+  variable: '--font-impact',
+  weight: '400',
+  display: 'swap',
+});
+
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+// tailwind.config.js
+
+import { Impact } from "@/app/fonts";
+
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",       // ajuste esses caminhos pro seu projeto!
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+theme: {
+    extend: {
+      fontFamily: {
+        impact: ['var(--font-impact)'],
+        gothic: ['var(--font-gothic)'],
+        oskons: ['var(--font-oskon)'],
+        lightdream: ['var(--font-lightdream)'],
+      },
+    },
+  },    
+  plugins: [],
+};
+
+export default config;
